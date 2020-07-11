3f63ead072a52c0485eddb9a8ea32792a1b7be99
Eoin Hackett <ehackett@gmail.com>
2020-07-11 18:21:29 +0100

    add styling for mobile layout

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const logoText = document.querySelector('#title');
+let containerWidth = document.querySelector('#landing').clientWidth;
+let titleSize = 96 * (containerWidth / 960);
+let blurbSize = 48 * (containerWidth / 960);
 
 function resizing() {
+  const logoText = document.querySelector('#title');
+  let containerWidth = document.querySelector('#landing').clientWidth;
+  let titleSize = 96 * (containerWidth / 960);
+  let blurbSize = 48 * (containerWidth / 960);
+  document.querySelector('#title').style.fontSize = `${titleSize}px`;
+  document.querySelector('#blurb').style.fontSize = `${blurbSize}px`;
   window.addEventListener('resize', () => {
-    let containerWidth = document.querySelector('#landing').clientWidth;
-    let titleSize = 96 * (containerWidth / 960);
-    let blurbSize = 48 * (containerWidth / 960);
     console.log(containerWidth, titleSize);
     document.querySelector('#title').style.fontSize = `${titleSize}px`;
     document.querySelector('#blurb').style.fontSize = `${blurbSize}px`;
