b620c6e841a2dab4c4ab07720bcc4ec94cc38898
Eoin Hackett <ehackett@gmail.com>
2020-07-22 15:51:28 +0100

    create eventlistener and getWeather function added eventListener on search button create getWeather function to retrieve info from api

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
-let targetLocation = document.querySelector('#search-box').textContent;
+let locationkey, searchLocation, searchLocationCountry;
+const apiKey = 'Ufs32bf0WathXSbMJGU1qV9kBL6EMR9u';
+
+let targetLocation = document.querySelector('#search-box').value;
 let searchButton = document.querySelector('#search-button ');
+
+searchButton.addEventListener('click', () => {
+  if (targetLocation === '') {
+    alert('Please enter a location');
+  } else {
+    getWeather(targetLocation);
+  }
+});
+
+// function using the location entered in the search box to retrieve weather info from accuweather api
+function getWeather(searchValue) {
+  let locationData = fetch(
+    `http://dataservice.accuweather.com/locations/v1/search?apikey=${apiKey}&q=${searchValue}`
+  );
+
+  locationData
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
