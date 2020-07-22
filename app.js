712db0c7398881f4af363d7c4008c2d63f8914f6
Eoin Hackett <ehackett@gmail.com>
2020-07-22 17:02:11 +0100

    create get5DayWeather function function uses location key to retrieve 5 day weather forecast from api

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let locationKey, searchLocation, searchLocationCountry;
+// let Headline, DailyForecasts;
 const apiKey = 'Ufs32bf0WathXSbMJGU1qV9kBL6EMR9u';
 
 let targetLocation = document.querySelector('#search-box').value;
@@ -12,7 +13,7 @@ searchButton.addEventListener('click', () => {
   }
 });
 
-// function using the location entered in the search box to retrieve weather info from accuweather api
+// function using the location entered in the search box to retrieve the location key
 function getLocationData(searchValue) {
   let locationData = fetch(
     `http://dataservice.accuweather.com/locations/v1/search?apikey=${apiKey}&q=${searchValue}`
@@ -23,10 +24,25 @@ function getLocationData(searchValue) {
     .then((data) => {
       locationKey = data[0].Key;
       searchLocationCountry = data[0].Country.ID;
+      get5DayWeather(locationKey);
     })
     .catch((err) => {
       console.log(err);
     });
 }
 
-function get5DayWeather(cityKey) {}
+// uses the location key retrieved in the getLocationData function to send a request to the api for the 5 day weather report
+function get5DayWeather(cityKey) {
+  let fiveDayData = fetch(
+    `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${apiKey}&metric=true`
+  );
+
+  fiveDayData
+    .then((response) => response.json())
+    .then((data) => {
+      displayWeatherData(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
