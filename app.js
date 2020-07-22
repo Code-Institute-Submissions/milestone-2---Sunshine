fcaf4da528ff9bdce79c2bddc4773d79c117e8ef
Eoin Hackett <ehackett@gmail.com>
2020-07-22 16:09:42 +0100

    create getLocationData function uses search loaction to retrieve location key location key is used in getWeather function to retrieve weather data

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let locationkey, searchLocation, searchLocationCountry;
+let locationKey, searchLocation, searchLocationCountry;
 const apiKey = 'Ufs32bf0WathXSbMJGU1qV9kBL6EMR9u';
 
 let targetLocation = document.querySelector('#search-box').value;
@@ -8,12 +8,12 @@ searchButton.addEventListener('click', () => {
   if (targetLocation === '') {
     alert('Please enter a location');
   } else {
-    getWeather(targetLocation);
+    getLocationData(targetLocation);
   }
 });
 
 // function using the location entered in the search box to retrieve weather info from accuweather api
-function getWeather(searchValue) {
+function getLocationData(searchValue) {
   let locationData = fetch(
     `http://dataservice.accuweather.com/locations/v1/search?apikey=${apiKey}&q=${searchValue}`
   );
@@ -21,9 +21,12 @@ function getWeather(searchValue) {
   locationData
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      locationKey = data[0].Key;
+      searchLocationCountry = data[0].Country.ID;
     })
     .catch((err) => {
       console.log(err);
     });
 }
+
+function get5DayWeather(cityKey) {}
