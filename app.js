f46c6a7678777870196509b48d7300b796f8e90c
Eoin Hackett <ehackett@gmail.com>
2020-07-31 22:08:17 +0100

    add dusokayFullweather function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let locationKey, searchLocation, searchLocationCountry;
 const apiKey = 'IYZf2eTw9XH3uBNLkfINMWL3DycoMsYx';
 let searchButton = document.querySelector('#search-button ');
+let landingContainer = document.querySelector('#landing');
 
 searchButton.addEventListener('click', () => {
   let targetLocation = document.querySelector('#search-box').value;
@@ -50,24 +51,22 @@ function display5DayWeatherData(weatherData) {
   console.log(weatherData);
   let ctaContainer = document.querySelector('.cta');
   let graphicContainer = document.querySelector('#graphic-container');
-  let headline = document.createElement('div');
+  let fullDetail = document.createElement('div');
   let fiveDay = document.createElement('div');
   let landingContainer = document.querySelector('#landing');
-  let weatherHeadline = weatherData.Headline;
+  // let weatherHeadline = weatherData.Headline;
   let weatherArray = weatherData.DailyForecasts;
-  let headlineDate = dayjs(weatherHeadline.EffectiveDate).format('D MMM YYYY');
+  // let headlineDate = dayjs(weatherHeadline.EffectiveDate).format('D MMM YYYY');
 
   graphicContainer.style.display = 'none';
   ctaContainer.style.display = 'none';
 
-  landingContainer.appendChild(headline);
-  headline.classList.add('headline');
   landingContainer.appendChild(fiveDay);
   fiveDay.classList.add('five-day');
 
-  console.log(weatherHeadline);
-  let headlineContent = `<h2>Headline for ${headlineDate}</h2><div class='headline-text'>${weatherHeadline.Text}</div>`;
-  headline.innerHTML = headlineContent;
+  // console.log(weatherHeadline);
+  // let headlineContent = `<h2>Headline for ${headlineDate}</h2><div class='headline-text'>${weatherHeadline.Text}</div>`;
+  // headline.innerHTML = headlineContent;
 
   weatherArray.forEach((e) => {
     let {
@@ -79,7 +78,7 @@ function display5DayWeatherData(weatherData) {
     } = e;
     let daily = document.createElement('div');
     daily.classList.add('weather-item');
-    let date = dayjs(e.Date).format('D MMM YYYY');
+    let date = dayjs(e.Date).format('dddd D/M/YYYY');
 
     fiveDay.appendChild(daily);
     daily.innerHTML = `<div class="date">${date}</div>
@@ -91,5 +90,48 @@ function display5DayWeatherData(weatherData) {
             <div class="precipitation"><h3>Precipitation</h3><p>${Day.PrecipitationIntensity} ${Day.PrecipitationType}</p></div>
             <div class="temp"><h3>Temperature</h3><p>Max: ${Maximum.Value}&#8451; Min: ${Minimum.Value}&#8451;</p></div>
             <button class="full-weather">More details</button>`;
+
+    let moreDetails = daily.querySelector('.full-weather');
+    moreDetails.addEventListener('click', () => {
+      displayFull(e);
+    });
   });
+}
+
+function displayFull(weatherObj) {
+  console.log(weatherObj);
+  let {
+    Date,
+    Temperature,
+    Temperature: { Minimum, Maximum },
+    Day,
+    Night,
+    Sun,
+    Moon,
+    HoursOfSun,
+  } = weatherObj;
+  let date = dayjs(weatherObj.Date).format('dddd D/M/YYYY');
+  let fullDetail = document.createElement('div');
+  landingContainer.prepend(fullDetail);
+  fullDetail.classList.add('headline');
+
+  fullDetail.innerHTML = `<div class="date">${date}</div>
+            <div class="icon-description">
+              <img src="/assets/img/icons/${
+                Day.Icon
+              }-s.png" alt="" class="icon" /><span class="description"
+                >${Day.IconPhrase}</span
+              >
+            </div>
+            <div class="precipitation"><h3>Precipitation</h3><p>${
+              Day.PrecipitationIntensity
+            } ${Day.PrecipitationType}</p></div>
+            <div class="temp"><h3>Temperature</h3><p>Max: ${
+              Maximum.Value
+            }&#8451; Min: ${Minimum.Value}&#8451;</p></div>
+            <div><h3>Sun</h3><div>Hours of Sun: ${HoursOfSun}</div><div>Sunrise: ${dayjs(
+    Sun.Rise
+  ).format('HH:mm')} Sunset: ${dayjs(Sun.Set).format(
+    'HH:mm'
+  )}</div></div><div></div><div></div><div></div>`;
 }
